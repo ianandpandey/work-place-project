e5d4aa833e4e691fc17cab06f995f7d53bf61c0c
ianandpandey <ianandpandey99@gmail.com>
2022-11-13 12:36:36 +0530

    first commit

---
+++ src/Components/AuthPage/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AuthPage() {
+  return (
+    <div>Auth</div>
+  )
+}
+
+export default AuthPage
---
+++ src/firebaseConfig/index.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB0YKQZB_MoV5qN6fAxqcgtcuJ7PTtCNFs",
+  authDomain: "work-place-9cab0.firebaseapp.com",
+  projectId: "work-place-9cab0",
+  storageBucket: "work-place-9cab0.appspot.com",
+  messagingSenderId: "664497270089",
+  appId: "1:664497270089:web:9a6be23a20f595b1413e98",
+  measurementId: "G-C5PRBMKMHJ",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
